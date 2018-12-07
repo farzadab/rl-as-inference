e8f565476a1916ff1f22ce6c80f95663fcf57048
Jon Lavington <jola2372@colorado.edu>
2018-12-06 21:23:34 -0800

    Update svi_non_uniform_action.py

--- svi/svi_non_uniform_action.py
+++ svi/svi_non_uniform_action.py
@@ -15,38 +15,46 @@ from envs.pointmass import PointMass
 
 from vectorized_policy_gradient import *
 
-T = 50
+def rl_model(env, last_model=None, T = 50, samples = 3):
 
-def rl_model(env, last_model=None):
-    # run simulation with random actions from the prior
-    crew = 0
-    state = env.reset()
-    mx_pos = env.max_position * th.ones(2)
-    env.state[:2] = pyro.sample("s_0", dist.Uniform(-1 * mx_pos, mx_pos))
-    env.state[2:] = 0
-    for i in range(T):
+    for trial in range(samples):
 
-        # check how we are sampling actions from their prior
-        if last_model == None:
-            # no beliefs about optimal action then a wide normal
-            a = pyro.sample("a_%d" % i, dist.Normal(th.zeros(2), 10 * th.ones(2)))
-        else:
-            # if we have an old model, sample from that (assumed to be dist.Normal(state*W+b, Sigma) though)
-            a = pyro.sample("a_%d" % i, last_model(state))
+        # run simulation with random actions from the prior
+        crew = 0
+        state = env.reset()
+        mx_pos = env.max_position * th.ones(2)
+        env.state[:2] = pyro.sample("s_0%d" % trial, dist.Uniform(-1 * mx_pos, mx_pos))
+        env.state[2:] = 0
 
-        state, r, _, _ = env.step(a.reshape(-1).detach().numpy())  # ignoring the `done` signal ...
-        crew += r
-        O_dist = dist.Bernoulli(th.FloatTensor([r]).exp())
-        pyro.sample("O_%d" % i, O_dist, obs=1)
+        for i in range(T):
+            # check how we are sampling actions from their prior
+            if last_model == None:
+                # no beliefs about optimal action then a wide normal
+                a = pyro.sample("a_" + str(trial) + str(i), dist.Normal(th.zeros(2), 10 * th.ones(2)))
+            else:
+                # if we have an old model, sample from that (assumed to be dist.Normal(state*W+b, Sigma) though)
+                a = pyro.sample("a_" + str(trial) + str(i), last_model(state))
+
+            state, r, _, _ = env.step(a.reshape(-1).detach().numpy())  # ignoring the `done` signal ...
+            crew += r
+            O_dist = dist.Bernoulli(th.FloatTensor([r]).exp())
+            pyro.sample("O_" + str(trial) + str(i), O_dist, obs=1)
 
     # TODO figure out: what should the return value be? I just used cumulative reward for now
     return th.FloatTensor([crew])
 
 
-def rl_linear_guide(env, critic, render=False):
+def rl_linear_guide(env, critic, T = 250, W_ = None, b_ = None, render=False):
+
     # Define net params, just a linear model right now:
-    W = pyro.param("W", th.randn((6, 2)))
-    b = pyro.param("b", th.randn((1, 2)))
+    if W_ == None:
+        W = pyro.param("W", th.randn((6, 2)))
+    else:
+        W = pyro.param("W", W_)
+    if b_ == None:
+        b = pyro.param("b", th.randn((1, 2)))
+    else:
+        b =  pyro.param("b", b_)
 
     # TODO: In section 3.2 he mentions that the distribution of transitions in q must be fixed to p
     # so I think it means we should run the simulation inside the guide too (I don't know how to do it otherwise)
@@ -77,7 +85,7 @@ def rl_linear_guide(env, critic, render=False):
         crew += r
     return th.FloatTensor([crew])
 
-def train_regression(iterations = 100, old_model = None):
+def train_regression(iterations = 100, T = 50, samples = 10, old_model = None, W = None, b = None):
 
     pyro.clear_param_store()
 
@@ -93,14 +101,15 @@ def train_regression(iterations = 100, old_model = None):
     )
 
     if old_model == None:
-        rl_linear_guide_ = lambda env: rl_linear_guide(env, critic)
+        rl_linear_guide_ = lambda env: rl_linear_guide(env, critic, 50, W, b)
+        rl_model_ = lambda env: rl_model(env, None, T, samples)
         svi = pyro.infer.SVI(model=rl_model,
                              guide=rl_linear_guide_,
                              optim=pyro.optim.Adam({"lr": 0.001}),
                              loss=pyro.infer.Trace_ELBO(num_particles=20))
     else:
-        rl_linear_guide_ = lambda env: rl_linear_guide(env, critic)
-        rl_model_ = lambda env: rl_model(env, old_model)
+        rl_linear_guide_ = lambda env: rl_linear_guide(env, critic, 50, W, b)
+        rl_model_ = lambda env: rl_model(env, old_model, T, samples)
         svi = pyro.infer.SVI(model=rl_model_,
                              guide=rl_linear_guide_,
                              optim=pyro.optim.Adam({"lr": 0.001}),
@@ -125,7 +134,8 @@ def train_regression(iterations = 100, old_model = None):
     # print('b2:', b)
 
     # # Visualizing the learned policy
-    # rl_linear_guide(env, critic, True)
+    input("Press Enter to see what a trajectory looks like...")
+    rl_linear_guide(env, critic, T, None, None, True)
     # rl_linear_guide(env, critic, True)
     # rl_linear_guide(env, critic, True)
     #
@@ -139,18 +149,33 @@ def train_regression(iterations = 100, old_model = None):
     # # time.sleep(100)
 
     return W, b
-# now we have a guess of the model, lets try it using the old waits
+
 
 def main():
 
+    # trajectory length
+    iterations = 10
+    T = 250
+    samples = 25
+    N = 10
+
     # get an initial set of values
-    W, b = train_regression(100)
+    W, b = train_regression(iterations, T, samples)
+
     # now lets try re-using our old guess as the new proir on actions
-    for iter in range(1,5):
+    for iter in range(1,N):
         # still use wide variance to start though
-        old_model = lambda state: dist.Normal(
-            th.FloatTensor(state).reshape(1,-1).mm(W) + b, 10/(iter**2)*th.ones(2))
-        W, b = train_regression(100, old_model)
+        W_new, b_new = train_regression(iterations, T, samples)
+        W = W + W_new
+        b = b + b_new
+
+    # divide to get average sample mean
+    W = W / N
+    b = b / n
+
+    print('W:', W)
+    print('b:', b)
 
 if __name__ == '__main__':
     main()
+
