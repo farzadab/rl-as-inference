b7df8f95a8578f21c35b0c76c4ca938f08a7df98
Ainaz Hajimoradlou <ainaz@cs.ubc.ca>
2018-12-11 13:16:49 -0800

    created the custom loss for PPO

--- svi/ppo.py
+++ svi/ppo.py
@@ -1,15 +1,19 @@
 # from pyro.infer.tracegraph_elbo import _compute_downstream_costs
-from svi.trace_elbo import _compute_log_r
-from pyro.infer.tracegraph_elbo import _get_baseline_options
 import torch as th
 import pyro
-from pyro.infer.util import (detach_iterable, torch_backward)
-from pyro.inder.elbo import ELBO
+import copy
+import pyro.poutine as pp
+from pyro.infer.util import (detach_iterable, torch_backward, zero_grads)
+from pyro.infer.elbo import ELBO
+from .trace_elbo import _compute_log_r
+from pyro.infer.tracegraph_elbo import _get_baseline_options
+from pyro.distributions.util import is_identically_zero
 
 epsilon = 0.2
 
 def _baseline_cost(node, downstream_cost, guide_site):
-    basline = 0.0
+    baseline_loss = 0.0
+    baseline = 0.0
     (nn_baseline, nn_baseline_input, use_decaying_avg_baseline, baseline_beta,
         baseline_value) = _get_baseline_options(guide_site)
     use_nn_baseline = nn_baseline is not None
@@ -42,35 +46,74 @@ def _baseline_cost(node, downstream_cost, guide_site):
     return downstream_cost, baseline_loss
     
 
-def PPO_TraceGraph_loss(model_trace, guide_trace, pre_guide_trace, weight):
+def PPO_TraceGraph_loss(model_trace, guide_trace, old_policy_log_prob, epsilon=0.2):
     surrogate_loss = 0
     baseline_loss = 0
-    downstream_costs = _compute_log_r(model_trace, pre_guide_trace)
+    downstream_costs = _compute_log_r(model_trace, guide_trace)
+    non_reparam = set(guide_trace.nonreparam_stochastic_nodes)
 
-    for name, guide_site in reverse(list(pre_guide_trace.nodes.items())):
-        score_func_term = guide_trace.nodes[name]["score_parts"].score_function /
-                            guide_site["score_parts"].score_function
+    for name, guide_site in guide_trace.nodes.items():
+        if guide_site["type"] == "sample" and (name in non_reparam):
+            score_func_term = guide_site["score_parts"].score_function
+            if not is_identically_zero(old_policy_log_prob[name]):
+                score_func_term = score_func_term - old_policy_log_prob[name].detach()
 
-        downstream_cost = downstream_costs[name]
-        downstream_cost, b_loss = _baseline_cost(name, downstream_cost, guide_site)
-        baseline_loss += b_loss
-        surrogate_loss += (th.clamp(score_func_term, min=1-epsilon, max=1+epsilon) *
-                            downstream_cost.detach()).sum()
+            downstream_cost, b_loss = _baseline_cost(name, downstream_costs[name], guide_site)
+            
+            if not is_identically_zero(score_func_term):
+                surrogate_loss += ((th.clamp(th.Tensor(score_func_term), min=1-epsilon, max=1+epsilon)) * \
+                                    downstream_cost.detach()).sum()
+            baseline_loss += b_loss
 
     trainable_params = any(site["type"] == "param"
                                for trace in (model_trace, guide_trace)
                                for site in trace.nodes.values())
     if trainable_params:
-        loss = weight * (-surrogate_loss + baseline_loss)
+        loss = (-surrogate_loss + baseline_loss)
         torch_backward(loss, retain_graph=True)
+        # torch_backward(loss)
+        # loss.backward()
+    # import ipdb ; ipdb.set_trace()
+    return loss
+
+def PPO_update(optimizer, model, guide, policy, args):
+    # weight = 1./num_particles
+    # losses = []
+    num_particles = args.nb_particles
+    total_loss = 0.0
+    old_policy = copy.deepcopy(policy)
+    # model_trace, guide_trace = ELBO._get_traces(model, guide, policy,)
+    guide_trace = pp.trace(guide).get_trace(policy, args)
+    model_trace = pp.trace(pp.replay(model, trace=guide_trace)).get_trace(policy, args)
+    guide_trace.compute_score_parts()
+    model_trace.compute_log_prob()
+
+    old_policy_log_prob = {name: site["score_parts"].score_function
+                                for name, site in guide_trace.nodes.items() 
+                                    if site["type"] == "sample"}
+    # import ipdb; ipdb.set_trace()
     
-    return loss
-
-def PPO_loss(model, guide, pre_guide_trace, num_particles, env):
-    weight = 1./num_particles
-    loss = 0.0
-    for model_trace, guide_trace in ELBO._get_traces(model, guide, env):
-        loss += PPO_TraceGraph_loss(model_trace, guide_trace, pre_guide_trace, weight)
-        pre_guide_trace = guide_trace
-
-    return loss
+    # with pp.trace(param_only=True) as param_capture:
+    #     params = set(site["value"].unconstrained()
+    #                     for site in param_capture.trace.nodes.values())
+    params = set(site["value"].unconstrained() 
+                    for name, site in guide_trace.nodes.items()
+                        if site["type"] == "param")
+    # import ipdb; ipdb.set_trace()
+    
+    for i in range(num_particles):
+        # optimizer.zero_grad()
+        zero_grads(params)
+        loss = PPO_TraceGraph_loss(model_trace, guide_trace, old_policy_log_prob) # calls backward
+        # losses.append(loss)
+        total_loss += loss.item()
+        # optimizer.step()
+        optimizer(params)
+        guide_trace = pp.trace(guide).get_trace(policy, args)
+        guide_trace.compute_score_parts()
+        model_trace = pp.trace(pp.replay(model, trace=guide_trace)).get_trace(policy, args)
+        model_trace.compute_log_prob()
+        # import ipdb; ipdb.set_trace()
+    # the new policy should be in guide_trace 'W' 'b'
+    #return new_policy
+    return total_loss/num_particles
--- svi/svi_test_mine.py
+++ svi/svi_test_mine.py
@@ -1,6 +1,13 @@
+'''
+SVI tests for RL
+'''
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+import shutil
 import time
+import json
+import os
 
 import torch as th
 import torch.nn as nn
@@ -12,165 +19,182 @@ import pyro.optim
 import pyro.distributions as dist
 
 from pyro.distributions.testing.fakes import NonreparameterizedNormal
-import pyro.poutine
 
 from envs.pointmass import PointMass
-# from .tracegraph_elbo import TraceGraph_ELBO
-from .distributions import FlexibleBernoulli
+from .tracegraph_elbo import TraceGraph_ELBO
+from .distributions import UnnormExpBernoulli, InfiniteUniform
+from .ppo import PPO_update
 
-T = 20
 
-def rl_model(env):
-    # run simulation with random actions from the prior
+def init_state():
+    mx_pos = th.ones(2)
+    p = pyro.sample("s", dist.Uniform(-1 * mx_pos, mx_pos))
+    g = pyro.sample("g", dist.Uniform(-1 * mx_pos, mx_pos))
+    return p, g
+
+def compute_reward(a, p, g):
+    return -1 * (a - (g - p)).pow(2).sum()
+
+def rl_model(_, args):
     crew = 0
-    env.reset()
-    mx_pos = env.max_position * th.ones(1)
-    env.state[:1] = pyro.sample("s_0", dist.Uniform(-1 * mx_pos, mx_pos))
-    env.state[1:] = 0
-    for i in range(T):
-        a = pyro.sample("a_%d" % i, dist.Uniform(-100 * th.ones(2), 100 * th.ones(2)))
-        # a = pyro.sample("a_%d" % i, dist.Normal(th.zeros(2), 10 * th.ones(2)))
-        p_state = env.state
-        _, r, _, _ = env.step(a.reshape(-1).detach().numpy())  # ignoring the `done` signal ...
-        crew += r
-        O_dist = FlexibleBernoulli(th.FloatTensor([r/T]).exp())
+    p, g = init_state()
+    for i in range(args.ep_len):
+        a = pyro.sample("a_%d" % i, InfiniteUniform(2))
+        # reward is the distance to the correct direction
+        r = compute_reward(a, p, g)
+        O_dist = UnnormExpBernoulli(th.FloatTensor([r / args.ep_len]))
         pyro.sample("O_%d" % i, O_dist, obs=1)
-    # O_dist = dist.Bernoulli(th.FloatTensor([crew/T]).exp())
-    # pyro.sample("O_%d" % i, O_dist, obs=1)
-    
-    # TODO figure out: what should the return value be? I just used cumulative reward for now
-    # print('reward in model: ', crew)
-    return th.FloatTensor([crew])
+        crew += r
+
+    return th.cat([a.squeeze().detach(), th.FloatTensor([crew])])
     
 
-def rl_linear_guide(env, render=False):
-    # Define net params, just a linear model right now:
-    # W = pyro.param("W", th.randn((3, 1)))
-    # b = pyro.param("b", th.randn((1, 1)))
-    pyro.module("policy", policy)
-    pyro.module("critic", critic)
+def rl_guide(policy, args):
+    pyro.module('policy', policy)
 
-    # TODO: In section 3.2 he mentions that the distribution of transitions in q must be fixed to p
-    # so I think it means we should run the simulation inside the guide too (I don't know how to do it otherwise)
-    # run simulation with "guided" actions
     crew = 0
-    env.reset()
-    # TODO: find better way to sample states ...
-    mx_pos = env.max_position * th.ones(1)
-    env.state[:1] = pyro.sample("s_0", dist.Uniform(-1 * mx_pos, mx_pos))
-    env.state[1:] = 0
-    s = env.state
-    for i in range(T):
-        if render:
-            env.render()
+    p, g = init_state()
+
+    for i in range(args.ep_len):
         a = pyro.sample(
             "a_%d" % i,
             NonreparameterizedNormal(
-                # (th.FloatTensor(s).reshape(1,-1).mm(W) / env.max_position + b) * env.max_torque,
-                policy(th.FloatTensor(s) / env.max_position) * env.max_torque,
-                .5
+                policy(th.cat([p, g])),
+                args.policy_stdev
             ),
-            infer={'baseline': {
-                'nn_baseline': critic,
-                'nn_baseline_input': th.cat([th.FloatTensor(s), th.FloatTensor([i])]).detach()
-            }},
         )
-        s, r, _, _ = env.step(a.detach().reshape(-1).numpy())  # ignoring the `done` signal ...
+        r = compute_reward(a, p, g)
         crew += r
-    # print('reward in guide: ', crew)
-    return th.FloatTensor([crew])
+
+    return th.cat([a.squeeze().detach(), th.FloatTensor([crew])])
 
 
+def create_policy_net(nb_layers, layer_size):
+    return nn.Sequential(*
+        [nn.Linear(4, layer_size), nn.LeakyReLU(),]
+        + sum([
+            [nn.Linear(layer_size, layer_size), nn.LeakyReLU()]
+            for _ in range(nb_layers)
+        ], [])
+        + [nn.Linear(layer_size, 2)]
+    )
 
-pyro.clear_param_store()
-env = PointMass(reward_style='pot')   # the 'pot-mvel' reward is always negative
 
-# Initializing the critic network (a.k.a. the value function)
-critic = nn.Sequential(
-    nn.Linear(4, 8),
-    nn.LeakyReLU(),
-    nn.Linear(8, 8),
-    nn.LeakyReLU(),
-    nn.Linear(8, 8),
-    nn.LeakyReLU(),
-    nn.Linear(8, 8),
-    nn.LeakyReLU(),
-    nn.Linear(8, 2),
-)
+def plot_elbo(losses):
+    p, = plt.plot(losses)
+    plt.title("negative ELBO")
+    plt.xlabel("step")
+    plt.ylabel("loss")
+    p.get_figure().canvas.draw()
+    p.get_figure().canvas.flush_events()
+    plt.show()
 
-# policy network
-policy = nn.Sequential(
-    nn.Linear(3, 8),
-    nn.LeakyReLU(),
-    nn.Linear(8, 8),
-    nn.LeakyReLU(),
-    nn.Linear(8, 8),
-    nn.LeakyReLU(),
-    nn.Linear(8, 8),
-    nn.LeakyReLU(),
-    nn.Linear(8, 1),
-)
 
-losses = []
-optimizer = pyro.optim.Adam({"lr": 0.001})
-loss_fn = pyro.infer.TraceGraph_ELBO(num_particles=20)
+def save_everything(args, policy, losses):
+    path = os.path.join('runs', '%s__%s' % (time.strftime("%Y-%m-%d_%H-%M-%S"), args.exp_name))
+    os.makedirs(path, exist_ok=True)
+    th.save(policy.state_dict(), os.path.join(path, 'policy.pt'))
+    plt.savefig(os.path.join(path, 'elbo.png'))
+    shutil.copy2(__file__, os.path.join(path, os.path.basename(__file__)))
+    with open(os.path.join(path, 'args.json'), 'w') as fargs:
+        json.dump(vars(args), fargs, indent=4)
+    with open(os.path.join(path, 'losses.json'), 'w') as flosses:
+        json.dump(losses, flosses, indent=4)
+    # time.sleep(1000)
 
-with pyro.poutine.trace(param_only=True) as param_capture:
-        loss = loss_fn.loss_and_grads(rl_model, rl_linear_guide, env)
-losses.append(loss)
-params = set(site["value"].unconstrained()
-        for site in param_capture.trace.nodes.values())
-optimizer(params)
-pyro.infer.util.zero_grads(params)
-pre_guide_trace = pyro.poutine.trace(rl_linear_guide).get_trace(env)
-# pre_guide_trace = loss_fn._get_traces(rl_model, rl_linear_guide, env)
 
-import ipdb; ipdb.set_trace()
-# for t in range(1, 10):
-#     # step() takes a single gradient step and returns an estimate of the loss
-#     # losses.append(svi.step(env))
-#     # print('\rStep %d' % (t+1), end='')
-#     with pyro.poutine.trace(param_only=True) as param_capture:
+def get_args():
+    parser = argparse.ArgumentParser(description="Simple test3: SVI for RL")
+    parser.add_argument("--load_path", type=str, default='')
+    parser.add_argument("--manual_debug", type=bool, default=False)
+    parser.add_argument("--exp_name", type=str, default=os.path.splitext(os.path.basename(__file__))[0])
+    parser.add_argument("--nb_layers", type=int, default=4)
+    parser.add_argument("--layer_size", type=int, default=16)
+    parser.add_argument("--nb_steps", type=int, default=1000)
+    parser.add_argument("--nb_particles", type=int, default=100)
+    parser.add_argument("--lr", type=float, default=0.0005)
+    parser.add_argument("--policy_stdev", type=float, default=0.5)
+    parser.add_argument("--ep_len", type=int, default=10)
+    return parser.parse_args()
+
+
+def train(args):
+    policy = create_policy_net(nb_layers=args.nb_layers, layer_size=args.layer_size)
+
+    pyro.clear_param_store()
+    # svi = pyro.infer.SVI(model=rl_model,
+    #                      guide=rl_guide,
+    #                      optim=pyro.optim.Adam({"lr": args.lr}),
+    #                      loss=pyro.infer.Trace_ELBO(num_particles=args.nb_particles))
+
+    losses = []
+    for t in range(args.nb_steps):
+        # step() takes a single gradient step and returns an estimate of the loss
+        # losses.append(svi.step(policy, args))
+        loss = PPO_update(pyro.optim.Adam({"lr": args.lr}),
+                            rl_model,
+                            rl_guide,
+                            policy,
+                            args)
+        losses.append(loss)
+        print('\rStep %d' % (t+1), end='')
+        # print('>>> loss: %f' % loss.item())
+        # print(guide_trace.nodes['a_1']['value'])
+        # import ipdb; ipdb.set_trace()
+
+    plot_elbo(losses)
+    save_everything(args, policy, losses)
+
+
+def load(args):
+    policy = create_policy_net(nb_layers=args.nb_layers, layer_size=args.layer_size)
+    policy.load_state_dict(
+        th.load(os.path.join(args.load_path, 'policy.pt'))
+    )
+    if args.manual_debug:
+        import ipdb
+        ipdb.set_trace()
+    rewards = []
+    for i in range(args.nb_steps):
+        rewards.append(rl_guide(policy, args)[-1].item())
+    print(sum(rewards) / args.ep_len / args.nb_steps)
+
+
+def main():
+    args = get_args()
+    if args.load_path:
+        load(args)
+    else:
+        train(args)
+
+
+if __name__ == '__main__':
+    main()
+
+
+# losses = []
+# optimizer = pyro.optim.Adam({"lr": 0.001})
+# loss_fn = pyro.infer.TraceGraph_ELBO(num_particles=20)
+
+# with pyro.poutine.trace(param_only=True) as param_capture:
 #         loss = loss_fn.loss_and_grads(rl_model, rl_linear_guide, env)
-#     losses.append(loss)
+# losses.append(loss)
+# params = set(site["value"].unconstrained()
+#         for site in param_capture.trace.nodes.values())
+# optimizer(params)
+# pyro.infer.util.zero_grads(params)
+# pre_guide_trace = pyro.poutine.trace(rl_linear_guide).get_trace(env)
+# # pre_guide_trace = loss_fn._get_traces(rl_model, rl_linear_guide, env)
 
-#     params = set(site["value"].unconstrained()
-#             for site in param_capture.trace.nodes.values())
-#     optimizer(params)
-#     pyro.infer.util.zero_grads(params)
-#     # losses.append(loss)
-#     # optimizer.step()
-#     # optimizer.zero_grad()
-#     pre_guide_trace = pyro.poutine.trace(rl_linear_guide).get_trace(env)
-#     # model_trace = pyro.poutine.trace(rl_model).get_trace(env)
-#     # import ipdb; ipdb.set_trace()
-#     print('------Step %d' % (t+1))
-#     print(guide_trace.nodes['a_10']['value'])
-#     print(loss)
-
-# W2 = pyro.param("W2")
-# b2 = pyro.param("b2")
-W = pyro.param("W")
-b = pyro.param("b")
-print('W:', W)
-print('b:', b)
-    
-# print('W2:', W)
-# print('b2:', b)
-
-# Visualizing the learned policy
-rl_linear_guide(env, True)
-rl_linear_guide(env, True)
-rl_linear_guide(env, True)
-rl_linear_guide(env, True)
-rl_linear_guide(env, True)
-rl_linear_guide(env, True)
-
-p, = plt.plot(losses)
-plt.title("ELBO")
-plt.xlabel("step")
-plt.ylabel("loss")
-p.get_figure().canvas.draw()
-p.get_figure().canvas.flush_events()
-plt.show()
-# time.sleep(100)
+# policy = nn(...)
+# for iter in nb_steps:
+#     old_policy = copy(policy)
+#     traces = get_traces(nb_particles)
+#     old_log_prob = traces.log_prob
+#     for i in nb_optim_steps:
+#         for j in batches:
+#             ratio = policy.log_prob(traces[batch]) - old_policy.log_prob(traces[batch]).detach()
+#             ## T * nb_particles matrix 
+#             adv = traces.downstream_costs[batch]
+#             loss = ratio * adv
+#             loss.optim()
